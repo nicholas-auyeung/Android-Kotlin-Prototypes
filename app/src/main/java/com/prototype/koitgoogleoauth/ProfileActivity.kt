48ed0d9536846372c39504f8a380316996dcb949
Nicholas <niauyeung@gmail.com>
2021-11-16 18:14:13 -0800

    repos created

---
+++ app/src/main/java/com/prototype/koitgoogleoauth/ProfileActivity.kt
@@ -0,0 +1,48 @@
+package com.prototype.koitgoogleoauth
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.google.firebase.auth.FirebaseAuth
+import com.prototype.koitgoogleoauth.databinding.ActivityProfileBinding
+
+class ProfileActivity : AppCompatActivity() {
+
+    //view binding
+    private lateinit var binding: ActivityProfileBinding
+
+    //firebase auth
+    private lateinit var firebaseAuth: FirebaseAuth
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityProfileBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        //init firebase auth
+        firebaseAuth = FirebaseAuth.getInstance()
+
+        //handle click, logout user
+        binding.logoutBtn.setOnClickListener{
+            firebaseAuth.signOut()
+            checkUser()
+        }
+    }
+
+    private fun checkUser(){
+        //get current user
+        val firebaseUser = firebaseAuth.currentUser
+        if(firebaseUser == null){
+            //user not logged in
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+        }else{
+            //user logged in
+            //get user info
+            val email = firebaseUser.email
+            //set email
+            binding.emailTv.text = email
+        }
+
+    }
+}
